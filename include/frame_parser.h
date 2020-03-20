bbcf47d73c17daac4be13e0f20d30e0cae0335bd
zhangxuhuizju <zhangxuhuizju@gmail.com>
2020-03-20 12:23:36 +0800

    add comment in .h files

--- include/frame_parser.h
+++ include/frame_parser.h
@@ -4,9 +4,25 @@
 #include "header_info.h"
 #include "conn_manager.h"
 
-
-int parse_frame(char* buf, size_t len,uint32_t& conn_id,addr_port& srcaddr);
-
+/**
+   * \brief parse the frame received by recv thread
+   *
+   * \param buf the receiver buffer filled by system recvfrom() function 
+   * \param len the receiver buffer length
+   * \param conn_id the value-result argument. When the function return, it set to the conn_id of the target client
+   * \param srcaddr the source address of the receive packet 
+   * \return 0: recv a data pkt when not established.
+             1: recv a request for exist connnection.
+             2: recv a request from client.
+             3: recv a back SYN-ACK from server(not in the server).
+             4: server recv a pkt with reply-syn-ack.
+             5: recv a scp redundent ack.
+             6: recv a scp packet ack.
+             7: recv a scp data packet.
+             8: recv a keep alive packet.
+             9: recv a scp close packet.
+            -1: recv an illegal packet.
+   */
+int parse_frame(char *buf, size_t len, uint32_t &conn_id, addr_port &srcaddr);
 
 #endif
-
--- include/scp_interface.h
+++ include/scp_interface.h
@@ -1,8 +1,60 @@
 #include "packet_generator.h"
 #include "frame_parser.h"
 
+/**
+   * \brief bind a local port to socket fd
+   *
+   * \param localip local network ip
+   * \param port bind port number
+   * \return -1 means failed, 0 means success
+   */
 int scp_bind(in_addr_t localip , uint16_t port);
+
+/**
+   * \brief init a socket and create the resend and clear thread
+   *
+   * \param tcpenable choose to add the Fake TCPheader or not
+   * \param isserver choose to init in server or in client device
+   * \return -1 means failed, 0 means success
+   */
 int init_rawsocket(bool tcpenable, bool isserver);
+
+/**
+   * \brief init a socket and create the resend and clear thread with Fake Tcpheader mode
+   *
+   * \param isserver choose to init in server or in client device
+   * \return -1 means failed, 0 means success
+   */
+int init_rawsocket(bool isserver);
+
+/**
+   * \brief connect to the server, only use in the client device
+   *
+   * \param remote_ip the connect server ip
+   * \param remote_port the connect server port
+   * \return 0 means failed, 1 means success
+   */
 int scp_connect(in_addr_t remote_ip,uint16_t remote_port);
+
+/**
+   * \brief send scp data
+   *
+   * \param buf the scp data content
+   * \param len send data length
+   * \see ConnManager::get_all_connections()
+   * \see ConnManager::get_conn()
+   * \see ConnidManager::local_conn_id
+   * \param fc the fakeConnection pointer
+   *           in server, use ConnManager::get_all_connections() to get all the connected client
+   *           or can use ConnManager::get_conn(uint32_t connid) to get the target client
+   *           in client, use ConnManager::get_conn(ConnidManager::local_conn_id) to get the target server
+   * \return if success, return the actual send length, 0 means the fc is not exist, -1 means failed 
+   */
 size_t scp_send(const char* buf,size_t len,FakeConnection* fc);
+
+/**
+   * \brief close the scp socket, clear the FakeConnection and the working thread
+   *
+   * \return 0 means success, -1 means failed
+   */
 int scp_close();
